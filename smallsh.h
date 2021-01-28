0a689e82c99d84a2ddb0936a449a1f70b28ebda5
Joseph Polaski <polaskij@os1.engr.oregonstate.edu>
2021-01-27 18:10:25 -0800

    Added shell prompt

---
+++ smallsh.h
@@ -0,0 +1,17 @@
+/*
+Joseph Polaski
+CS344: Program 3
+
+smallsh.h
+
+This headerfile contains all prototypes for functions defined in smallsh.c
+
+*/
+
+// header guard
+#ifndef SMALLSH_
+#define SMALLSH_
+
+char *getUserCommandLine(int maxLength); // prompts user and recieves command line
+
+#endif
