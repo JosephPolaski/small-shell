f8015a6e07c9e6f84b33ca6ed26ed83f8b32967b
Joseph Polaski <polaskij@os1.engr.oregonstate.edu>
2021-01-28 20:15:41 -0800

    Implemented Struct to organize commands from user. Still need to finish handling of args

--- smallsh.c
+++ smallsh.c
@@ -12,14 +12,26 @@ This is the main source file for the smallsh shell program.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "smallsh.h"
 
-int main(void)
-{   
-    const int MAXCMDLEN = 2048; // maximum characters allowed in command line
-    const int MAXARGS = 512; // maximum arguments allowed in command line
+// Global Constants
+#define MAXCMDLEN 2048 // maximum characters allowed in command line
+#define MAXARGS 512 // maximum arguments allowed in command line
 
-    char *userCmdLine; // will hold the command line from the user max length 2048 characters
+// This struct will be used to keep track of the user input commands
+struct userCommands
+{
+    char *cmdWithArgs[MAXARGS]; // will be an array of strings with the command and all arguments
+    char *inputFile; // will hold input file name if any
+    char *outputFile; // will hold output file if any
+    bool isBackground; // will the process run in the background?
+};
+
+int main(void)
+{       
+
+    char *userCmdLine; // will hold the original command line from the user max length 2048 characters
 
     // Establish Main Loop Flag
     enum progStatus {active, inactive};
@@ -30,6 +42,7 @@ int main(void)
     {   
         userCmdLine = getUserCommandLine(MAXCMDLEN);
 
+        // remember to free struct
         free(userCmdLine);
     }
 
@@ -47,9 +60,72 @@ int main(void)
 char *getUserCommandLine(int maxLength)
 {
     char *cmdLine = calloc(maxLength, sizeof(char)); // dynamically allocate memory for user input on the heap
+
     printf(": "); // print out smallsh prompt token to user
-    fgets(cmdLine, sizeof(cmdLine), stdin);
-    cmdLine[strlen(cmdLine)-1] = '\0'; // strip off ending \n from fgets
+    fflush(stdout); // flush
+
+    fgets(cmdLine, maxLength, stdin); // read in command line from user
 
     return (cmdLine);
+}
+
+/*
+*   buildCmdStruct
+*
+*   takes the raw user entered command line and builds it int an organized struct
+*
+*   :parameter: userCmdLine: command line entered by the user
+*   :return: cmdStruct: structure containing commands organized as data members
+*/
+struct userCommands *buildCmdStruct(char *userCmdLine)
+{       
+    // dynamically allocate memory for return structure
+    struct userCommands *cmdStruct = malloc(sizeof(struct userCommands));
+
+    int argIndex = 0; // will track the argument index for cmdStruct->cmdWIthArgs[]
+
+    // initialize optional data members
+    cmdStruct->inputFile = NULL;
+    cmdStruct->outputFile = NULL;
+    cmdStruct->isBackground = false;
+
+    // tokenize userCmdLine and read in all values
+    char *token;
+
+    // retrieve first token
+    token = strtok(userCmdLine, " ");
+
+    // keep parsing " " delimited strings until the end of the command line
+    while(token != NULL)
+    {   
+        // check for input file
+        if(token == "<")
+        {
+            token = strtok(NULL, " "); // skip to input file name
+            cmdStruct->inputFile = calloc(strlen(token) + 1, sizeof(char)); // dynamically allocate memory for token size string
+            strcpy(cmdStruct->inputFile, token); // copy string to inputFile data member
+        }
+        // check for output file
+        else if(token == ">")
+        {
+            token = strtok(NULL, " "); // skip to output file name
+            cmdStruct->outputFile = calloc(strlen(token) + 1, sizeof(char)); // dynamically allocate memory for token size string
+            strcpy(cmdStruct->outputFile, token); // copy string to outputFile data member
+        }
+        // check for background flag
+        else if(token == "&")
+        {
+            cmdStruct->isBackground = true; // set background flag to true
+        }
+        // treat as command or argument
+        else
+        {
+            // check token for expansion
+            // add to args array
+            // iterate counter
+        }
+
+        token = strtok(NULL, " "); // retreive next token
+    }
+    
 }
