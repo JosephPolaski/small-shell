3caf22e74b1ae8d2671fc0eb6ffc34e66a16be4d
Joseph Polaski <polaskij@os1.engr.oregonstate.edu>
2021-02-04 17:19:59 -0800

    Smallsh is fully functional

--- smallsh.h
+++ smallsh.h
@@ -14,14 +14,12 @@ This headerfile contains all prototypes for functions defined in smallsh.c
 
 char *getUserCommandLine(int maxLength); // prompts user and recieves command line
 struct userCommands *buildCmdStruct(char *userCmdLine); // build a struct out of the user entered command line
-void printStruct(struct userCommands *testStruct); // TEST PURPOSES ONLY
 int runUserCommands(struct userCommands *cmdStruct, int *lastProcStat, struct sigaction sigIntAction, int backgroundPIDs[]); // runs the commands entered by the user
 void executeOthers(struct userCommands *cmdStruct, int *lastProcStat, struct sigaction sigIntAction, int backgroundPIDs[]); // this function executes all non custom bash commands given
 void redirectIO(struct userCommands *cmdStruct); // this function handles any file redirection necessary based on user inputs
 void checkExitStatus(int lastFGStat); // checks and prints out the exit status of the last foreground process
 void trackBGPID(int pid, int backgroundPIDs[]); // will add a new background pid to the tracking list
 void removeBGPID(int pid, int backgroundPIDs[]); // removes a matching pid from the tracking list
-void checkBGProcs(void); // checks for finishing background processes
 void checkBackground(int backgroundPids[]);
 void SIGINT_Handler(int signum);
 void SIGSTP_Handler(int signo);
